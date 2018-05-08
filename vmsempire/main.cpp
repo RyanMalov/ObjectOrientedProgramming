465422734b4b698a6c3b44538d80f337554ffe30
RyanMalov <ram179@zips.uakron.edu>
2018-05-08 13:11:59 -0400

    Thought i pushed changes... didn't.... and had to rework some things. Still getting errors, but think i know what needs fixed

--- vmsempire/main.cpp
+++ vmsempire/main.cpp
@@ -2,8 +2,27 @@
 //Ryan Malov
 
 #include <iostream>
+#include "units.hpp"
 
-int main(int argv, char* argc[])
+
+int main(int argv, char** argc)
 {
+		Army army1(Owner);
+
+		std::cout << "Army was created!" << std::endl;
+		std::cout << "The owner is: " << army1.getOwner() << std::endl;
+		std::cout << "The Build Time is: " << army1.getBuildTime() << std::endl;
+		std::cout << "The Strength is: " << army1.getStrength() << std::endl;
+		std::cout << "The Health is: " << army1.getHP() << std::endl;
+		std::cout << "The Moves are: " << army1.getMoves() << std::endl;
+		std::cout << "The Range is: " << army1.getRange() << std::endl;
+		std::cout << "The Capaity is: " << army1.getCapacity() << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+
+		army1.Action(SENTRY);
+		army1.Action(MOVE_S);
+
+		return 0;
 
 }
--- vmsempire/units.hpp
+++ vmsempire/units.hpp
@@ -16,30 +16,30 @@ enum Owner
 //Define the unit pieces in the game
 enum UnitType
 {
-	Army,
-	Fighter,
-	Patrol,
-	Destroyer,
-	Submarine,
-	Transport,
-	Carrier,
-	Battleship,
-	Satellite,
-	NumObjects,
-	NoPiece
+	ARMY,
+	FIGHTER,
+	PATROL,
+	DESTROYER,
+	SUBMARINE,
+	TRANSPORT,
+	CARRIER,
+	BATTLESHIP,
+	SATELLITE,
+	NUMOBJECTS,
+	NOPIECE
 };
 
 //Defines the types of movement available
 enum UnitMovement
 {
-	North,
-	NorthEast,
-	East,
-	SouthEast,
-	South,
-	SouthWest,
-	West,
-	NorthWest
+	NORTH,
+	NORTHEAST,
+	EAST,
+	SOUTHEAST,
+	SOUTH,
+	SOUTHWEST,
+	WEST,
+	NORTHWEST
 };
 
 //Used for Special Movement
@@ -60,12 +60,10 @@ enum SpecMovement
 	WFTRANSPORT,
 	MOVE_N,
 	MOVE_NE,
-	Move_E,
-	Move_SE,
-	Move_S,
-	Move_SW,
-	Move_W,
-	Move_NW
+	MOVE_E,
+	MOVE_SE,
+	MOVE_W,
+	MOVE_NW
 };
 
 //Actions a unit can make
@@ -80,15 +78,92 @@ enum Action
 //Create the unit class. This will hold all global functions and variables
 class Unit
 {
-	Unit(Owner Own, Type t, int BTime, int Health, int Str, int Move, int Rang, int Capac)
-	: owner(Own), type(t), BuiltTime(BTime), HP(Health), Strength(Str), Moves(Move), Range(Rang), Capacity(Capac)
-	{}
+	public:
+
+		//Default constructor		
+		Unit(Owner Own,UnitType t, int BTime, int Health, int Str, int Move, int Rang, int Capac)
+		: owner(Own), type(t), BuildTime(BTime), HP(Health), Strength(Str), Moves(Move), Range(Rang), Capacity(Capac)
+		{}
+
+		virtual bool Action(const SpecMovement Command) = 0;
+
+		//Define Accessors for the units
+		Owner getOwner()
+		{
+			return owner;
+		}
+
+		int getBuildTime()
+		{
+			return BuildTime;
+		}
+
+		int getHP()
+		{
+			return HP;
+		}
+
+		int getStrength()
+		{
+			return Strength;
+		}
+
+		int getMoves()
+		{
+			return Moves;
+		}
+
+		int getRange()
+		{
+			return Range;
+		}
+
+		int getCapacity()
+		{
+			return Capacity;
+		}
+
+		//Define the Mutators for the units
+		void setOwner(const Owner Own)
+		{
+			this -> owner = Own;
+		}
+
+		void setBuildTime(const int BTime)
+		{
+			this -> BuildTime = BTime;
+		}
+
+		void setHP(const int Health)
+		{
+			this -> HP = Health;
+		}
+
+		void setStrength(const int Str)
+		{
+			this -> Strength = Str;
+		}
+
+		void setMoves(const int Move)
+		{
+			this -> Moves = Move;
+		}
+
+		void setRange(const int Rang)
+		{
+			this -> Range = Rang;
+		}
+
+		void setCapacity(const int Capac)
+		{
+			this -> Capacity = Capac;
+		}
 
 	private:
 		
 		//Unit Properties
 		Owner owner;		//Who owns the unit
-		Type type;			//What type of unit it is
+		UnitType type;		//What type of unit it is
 		int BuildTime;		//How long the build time is
 		int Strength;		//How strong a unit is
 		int HP;				//How much Health a unit has
@@ -108,14 +183,125 @@ class Unit
 		bool ArmyAttack();
 		bool Repair();
 		bool WFTransport();
-		bool Move(const Direction Dir);
+		bool Move(const UnitMovement Dir);
 };
 
+//Subclass for Army
+class Army : public Unit
+{
+	Army(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, ARMY, 5, 1, 1, 1, 0, 0), Location(locat)
+	{}
 
+	bool Action(const SpecMovement Command) override;
+	
+	//Used for the coordinates of the unit
+	std::pair<int, int> Location;
+};
 
+//Subclass for Fighter
+class Fighter : public Unit
+{
+	Fighter(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, FIGHTER, 10, 1, 1, 8, 32, 0), Location(locat)
+	{}
 
+	bool Action(const SpecMovement Command) override;
 
+	//Used for the coordinates of the unit
+	std::pair<int, int> Location;
+};
 
+//Subclass for Patrol
+class Patrol : public Unit
+{
+	Patrol(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, PATROL, 15, 1, 1, 4, 0, 0), Location(locat)
+	{}
+
+	bool Action(const SpecMovement Command) override;
+
+	//Coordinates
+	std::pair<int, int> Location;
+};
+
+//Subclas for Destroyer
+class Destroyer : public Unit
+{
+	Destroyer(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, DESTROYER, 20, 1, 3, 2, 0, 0), Location(locat)
+	{}
+
+	bool Action(const SpecMovement Command) override;
+
+	//Coordinates
+	std::pair<int, int> Location;
+};
+
+//Subclass for Submarine
+class Submarine : public Unit
+{
+	Submarine(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, SUBMARINE, 20, 3, 2, 2, 0, 0), Location(locat)
+	{}
+
+	bool Action(const SpecMovement Command) override;
+
+	//Coordinates
+	std::pair<int, int> Location;
+};
+
+//Subclass for Transport
+class Transport : public Unit
+{
+	Transport(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, TRANSPORT, 30, 1, 1, 2, 0, 6), Location(locat)
+	{}
+
+	bool Action(const SpecMovement Command) override;
+
+	//Coordinates
+	std::pair<int, int> Location;
+};
+
+//Subclass for Carrier
+class Carrier : public Unit
+{
+	Carrier(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, CARRIER, 30, 1, 8, 2, 0, 8), Location(locat)
+	{}
+
+	bool Action(const SpecMovement Command) override;
+
+	//Coordinates
+	std::pair<int, int> Location;
+};
+
+//Subclass for Battleship
+class Battleship : public Unit
+{
+	Battleship(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, BATTLESHIP, 40, 2, 10, 2, 0, 0), Location(locat)
+	{}
+
+	bool Action(const SpecMovement Command) override;
+
+	//Coordinates
+	std::pair<int, int> Location;
+};
+
+//Subclass for Satelite
+class Satellite : public Unit
+{
+	Satellite(Owner Own, std::pair<int, int> locat)
+	: Unit(Own, SATELLITE, 50, 0, 1, 10, 500, 0), Location(locat)
+	{}
+
+	bool Action(const SpecMovement Command) override;
+
+	//Coordinates
+	std::pair<int, int> Location;
+};
 
 
 
